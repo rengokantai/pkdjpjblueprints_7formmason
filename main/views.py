a72ca71a390fa1514e8b6cb435a913db51ead6d2
Hernan Y.Ke <yuriqiao@gmail.com>
2016-06-01 09:32:03 -0400

    Add custom form view

--- main/views.py
+++ main/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
 # Create your views here.
+import json
+from django import forms
+from django.views.generic import FormView
+
+class CustomFormView(FormView):
+    template_name = "custom_form.html"
+    def get_form(self, form_class=None):
+        form_structure_json = """{
+        "name":"string",
+        "age":"number",
+        "city":"string",
+        "country":"string",
+        "time_lived_in_current_city":"string"
+                }"""
+        form_structure = json.loads(form_structure_json)
+        custom_form = forms.Form(**self.get_form_kwargs())
+        for key,value in form_structure.items():
+            field_class = self.get_field_class_from_type(value)
+            if field_class is not None:
+                custom_form.fields[key] = field_class()
+            else:
+                raise TypeError("invalid {}".format(value))
+        return custom_form
+
+    def get_field_class_from_type(self,value_type):
+        if value_type == "string":
+            return forms.CharField
+        elif value_type == "number":
+            return forms.IntegerField
+        else:
+            return None
--- pkdjpjblueprints_7formmason/urls.py
+++ pkdjpjblueprints_7formmason/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from main.views import CustomFormView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$',CustomFormView.as_view(),name='custom_view')
 ]
