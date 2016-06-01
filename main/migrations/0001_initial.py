8d5bf1df8e31f4f29c0ad910dc3af112ecd4509a
Hernan Y.Ke <yuriqiao@gmail.com>
2016-06-01 09:42:23 -0400

    migrate

---
+++ main/migrations/0001_initial.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import jsonfield.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='FormSchema',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('title', models.CharField(max_length=100)),
+                ('schema', jsonfield.fields.JSONField()),
+            ],
+        ),
+    ]
--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from jsonfield import JSONField
 # Create your models here.
+
+class FormSchema(models.Model):
+    title = models.CharField(max_length=100)
+    schema = JSONField()
+
